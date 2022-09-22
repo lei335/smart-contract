9a1510d54e814f63e7aea2504b841ab837489550
zl <zl@memolabs.com>
2022-09-22 17:56:43 +0800

    add slot

---
+++ solidity/slot/Slot.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier:UNLICENSED
+pragma solidity 0.8.16;
+
+contract FacetA{
+    struct FacetData{
+        bytes32 data;
+    }
+
+    function facetData() internal pure returns (FacetData storage facet) {
+        bytes32 storagePosition = keccak256("Diamond:Storage:FacetA");
+        assembly {
+            facet.slot := storagePosition
+        }
+    }
+
+    function setDataA(bytes32 _data) external {
+        FacetData storage facet = facetData();
+        facet.data = _data;
+    }
+
+    function getDataA() external view returns (bytes32) {
+        FacetData storage facet = facetData();
+        return facet.data;
+    }
+}
